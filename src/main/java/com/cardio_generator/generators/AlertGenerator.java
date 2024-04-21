b24cfacfa833759de38b0d9a13bb1877af4248d1
akman <a.akman@student.maastrichtuniversty.nl>
2024-04-21 23:45:36 +0200

    Added java documentation for AlertGenerator

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -3,16 +3,30 @@ package com.cardio_generator.generators;
 import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+/**
+ * AlertGenerator class creates alerts based on some certain conditions
+ * It resolves or triggers alerts for a specific patient
+ */
 public class AlertGenerator implements PatientDataGenerator {
-
+    /**
+     * This is a random generator used to generate probabilities for alerts.
+     */
     public static final Random RANDOM_GENERATOR = new Random(); // I changed randomGenerator to RANDOM_GENERATOR because since it is a constant it should be in UPPER_SNAKE_CASE format
+    /**
+     * This one represents the state of alerts for each patient.
+     * False means resolved, true means pressed.
+     */
     private boolean[] alertStates; // false = resolved, true = pressed  // I changed the variable name according to camelCase format
 
     public AlertGenerator(int patientCount) {
         alertStates = new boolean[patientCount + 1]; // I changed the variable name according to camelCase format
     }
-
+    /**
+     * Generates alerts for a given patient with using random probabilities.
+     *
+     * @param patientId this is the ID of the patient
+     * @param outputStrategy this is the strategy used to output alerts
+     */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
